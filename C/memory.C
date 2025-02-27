f6c246ec745b2788d3e67cd6d05d9c32a3996d34
John Burns <jzburns@gmail.com>
2025-02-27 09:39:59 +0000

    cleaned up

--- C/memory.C
+++ C/memory.C
@@ -10,37 +10,26 @@
 const int sz = 6;
 unsigned array[ sz ] = { 9, 2, 3, 4, 8, 1 };
 unsigned someValue = 55;
-
-// we can think of someValue not as an unsigned in
-// but as the first element in an array of unsigned ints
 unsigned someValueInArrayForm[1] = { 55 };
 
 int main (int argc, char** argv) {
+	unsigned* p = array;
 
-	// we will use a pointer
-	// to access / modify array
-	unsigned* p;
-	p = array;
-
-	// let us update the 3rd value of the array
+	// update the 3rd value of the array
 	p += 3;
 	*p = 99;
+	printf("the 3rd element is is %u \n", *p);
 
-	for(int i = 0; i < sz; i++) {
-		printf("pos %d has value %u\n", i, array[i]);
-	}
- 
-	printf("someValue is %u \n", someValue);
-	// lets use the pointer to 
+	// use the pointer to 
 	// access / modify the integer someValue;
+	printf("someValue is %u \n", someValue);
 	p = &someValue;
 	*p = 123;
 	printf("now someValue is %u\n", someValue);
 
-	// now look at this ...
-	// what does it do?
+	// but what does this do?
 	p += 256;
 	*p = 333;
 
 	return 0;
-}
+}    
