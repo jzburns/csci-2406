4c0dcb9c99aa5a44548adf389c35499c7f51438f
JBurns <jzburns@gmail.com>
2025-06-29 15:23:50 +0100

    libc printf demo

--- arm-assembly/libc/output_string.s
+++ arm-assembly/libc/output_string.s
@@ -1,13 +1,30 @@
 .global main
 main:
-    	push    {lr} 
-	ldr r0, =output_string
-	mov r1, #1
+	// save the return address from main
+  push {lr} 
 
-	// use puts from libc
+	// command line: number of params
+
+	// first parameter - program name
+	ldr r2, [ r1 ]
+	// second parameter - program parameters
+	ldr r3, [ r1, #4 ]
+
+	// we are finished accessing r1
+	// so we can prepare it for printf
+	mov r1, r0
+
+	ldr r0, =output_string
+
+	// use puts or printf from libc
 	bl printf
+
+	// set up mains return parameters
 	mov r0, #0
-	pop     {pc}
+
+	// now return from main
+	pop {pc}
 
 .data
-	output_string: .asciz "Hello %d there!\n"
+	output_string: 
+		.asciz "Command line params: num params: %d, program name %s, first param: %s\n"
