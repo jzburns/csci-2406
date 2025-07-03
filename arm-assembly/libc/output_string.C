6517ef05ddc54936b0b4aeb4254adc2b38b3436d
ARM32 User <arm32user@example.com>
2025-07-03 18:05:02 +0100

    renamed files

---
+++ arm-assembly/libc/output_string.C
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Command line params: num params: %d, program name %s, first param: %s\n", argc, argv[0], argv[1]);
+}
--- arm-assembly/libc/test.C
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char* argv[]) {
-	printf("Here is the first value %s\n", argv[1]);
-}
