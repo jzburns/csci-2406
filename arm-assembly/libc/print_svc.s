545dc745ec37ce06e250d7a1f459df07eb998369
JBurns <jzburns@gmail.com>
2025-07-04 17:58:02 +0400

    tidy up

--- arm-assembly/libc/print_svc.s
+++ arm-assembly/libc/print_svc.s
@@ -1,14 +1,14 @@
 .global main
 	main:
-  	push {lr}
+		push {lr}
 
-  	mov r0, #1
-  	ldr r1, =string
-  	mov r2, #12
-  	mov r7, #4
-  	svc #0
+		mov r0, #1
+		ldr r1, =string
+		mov r2, #12
+		mov r7, #4
+		svc #0
 
-  	pop {pc}
+		pop {pc}
 
 .data
 	string: .asciz "Hello World\n"
