c142e5e6ca246299cc6cda60340a38669ca555aa
JBurns <jzburns@gmail.com>
2025-07-04 17:49:56 +0400

    C code change

--- arm-assembly/libc/print.C
+++ arm-assembly/libc/print.C
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	printf("Command line params: num params: %d, program name %s, first param: %s\n", argc, argv[0], argv[1]);
+	printf("Command line params: num params: %d, \
+program name %s, \
+first param: %s\n", \
+	argc, argv[0], argv[1]);
 }
